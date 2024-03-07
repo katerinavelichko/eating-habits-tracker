c6886ed134b32e9400bb28dd64a00723ee1ad009
katerinavelichko <kkvelichko@gmail.com>
2024-03-07 18:18:41 +0300

    + main page

--- site/app/__init__.py
+++ site/app/__init__.py
@@ -8,3 +8,4 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL', 'postgresql://experimentals:experimentals1@db:5432/habits_db')
 
 
+from . import views
