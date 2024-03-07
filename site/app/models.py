30c9da3d0c56f81a32a68c14cbd6b340323aae8e
root <root@MSI>
2024-03-07 20:01:39 +0300

    added file models.py

---
+++ site/app/models.py
@@ -0,0 +1,27 @@
+from . import db
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+
+    @classmethod
+    def add_user(cls, name, email, password):
+        user = cls(name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        print(f'Добавлен новый пользователь: {name}')
+
+    @classmethod
+    def delete_user(cls, name, email):
+        user = cls.query.filter_by(name=email).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            print(f'Пользователь {name} был удален')
+        else:
+            print(f'Пользователя {name} нет в списках')
