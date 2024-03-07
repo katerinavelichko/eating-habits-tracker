ae4ae0c301f56b31bfb44c22dd06e38e314b7218
root <root@MSI>
2024-03-07 20:05:24 +0300

    updated requirments and __init__ file

--- site/app/__init__.py
+++ site/app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
-import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+from . import views
+import os
 
 app = Flask(__name__)
 DEBUG = True
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL', 'postgresql://experimentals:experimentals1@db:5432/habits_db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL',
+                                                       'postgresql://experimentals:experimentals1@db:5432/habits_db')
 
-
-from . import views
+# DataBase
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
