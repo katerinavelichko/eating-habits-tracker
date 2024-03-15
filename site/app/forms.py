d92e8a4e512f38f62ac0fcf3381b265e683d3228
root <root@MSI>
2024-03-15 15:24:44 +0300

    + sign up

---
+++ site/app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Email, Length, DataRequired
+
+
+class CreateUserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[Email(message="Invalid email address")])
+    password = PasswordField(
+        "Password", validators=[Length(min=8, message="Short password! min - 8")]
+    )
+    submit = SubmitField("Отправить")
